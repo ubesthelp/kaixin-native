ca9afb669292bfd4954d6c95206f7027793c6ce3
Roy QIU <karoyqiu@gmail.com>
2020-07-28 20:47:57 +0800

    添加未完成的登录函数。

--- src/kaixin.cpp
+++ src/kaixin.cpp
@@ -4,19 +4,20 @@
  * \brief       开心 C SDK 源文件。
  *
  * \version     0.1
- * \date        2018-3-4
+ * \date        2020-7-28
  *
  * \author      Roy QIU <karoyqiu@gmail.com>
- * \copyright   © 2018 Roy QIU。
+ * \copyright   © 2020 开心网络。
  *
  **************************************************************************************************/
 #include "kaixin.h"
 
-#include <cassert>
-#include <cerrno>
-#include <string>
+#include <functional>
+#include <sstream>
 #include <ixwebsocket/IXHttpClient.h>
-#include <rapidjson/document.h>
+#include <ixwebsocket/IXNetSystem.h>
+
+#include "rapidjsonhelpers.h"
 
  // 纠正 EINVAL 被重定义为 WSAEINVAL 的问题。
 #ifdef WIN32
@@ -24,8 +25,12 @@
 #define EINVAL 22
 #endif
 
+
 // 字符串到字符串的映射
 using string_map = ix::WebSocketHttpHeaders;
+
+// 响应数据处理函数类型
+using response_data_handler = std::function<int(const rapidjson::Value &)>;
 
 // 全局配置参数
 struct Config
@@ -42,28 +47,120 @@ struct Config
 
 static Config *g_config = nullptr;
 
-
+// 判断字符串是否为空
 static inline bool is_empty(const char *s)
 {
     return s == nullptr || strlen(s) == 0;
 }
 
-
-static int send_request(const std::string &verb, const std::string &path, const string_map &quries,
-                        const string_map &form)
+// 连接字符串
+template <typename Range, typename Value = typename Range::value_type>
+static std::string join(Range const &elements, const char *const delimiter)
 {
-    assert(!verb.empty() && !path.empty() && path.at(0) == '/');
-    auto url = g_config->base_url + path;
+    std::ostringstream os;
+    auto b = std::begin(elements), e = end(elements);
+
+    if (b != e)
+    {
+        std::copy(b, std::prev(e), std::ostream_iterator<Value>(os, delimiter));
+        b = std::prev(e);
+    }
+
+    if (b != e)
+    {
+        os << *b;
+    }
+
+    return os.str();
+}
+
+// 根据查询映射生成表单字符串
+static std::string make_form(const string_map &queries)
+{
+    static ix::HttpClient http;
+
+    if (queries.empty())
+    {
+        return {};
+    }
+
+    std::vector<std::string> form;
+    form.reserve(queries.size());
+
+    for (const auto &[key, value] : queries)
+    {
+        form.emplace_back(http.urlEncode(key + '=' + value));
+    }
+
+    return join(form, "&");
+}
+
+// 根据高 16 位和低 16 位拼成 32 位整数
+static inline int make_int(int upper, int lower)
+{
+    return static_cast<int>((static_cast<uint32_t>(upper & 0xffff) << 16) | (lower & 0xffff));
+}
+
+// 根据路径和查询映射生成完整 URL
+static std::string make_url(const std::string &path, const string_map &queries)
+{
+    auto url = g_config->base_url;
+    url += path;
+
+    if (!queries.empty())
+    {
+        url += '?' + make_form(queries);
+    }
+
+    return url;
+}
+
+// 同步发送请求并返回结果
+static int send_request(const std::string &verb, const std::string &path, const string_map &queries,
+                        const string_map &form, const response_data_handler &handler)
+{
+    assert(!verb.empty() && !path.empty() && path.at(0) == '/' && !!handler);
+
+    // 构造 URL
+    auto url = make_url(path, queries);
 
     ix::HttpClient http;
-    auto req = http.createRequest(url, verb);
+    auto args = http.createRequest();
 
     if (!g_config->id_token.empty())
     {
-        req->extraHeaders.insert(std::make_pair("Authorization", "Bearer " + g_config->id_token));
+        // 设置认证头
+        args->extraHeaders.insert(std::make_pair("Authorization", "Bearer " + g_config->id_token));
     }
 
-    return 0;
+    // 构造请求体，并发送请求
+    auto body = make_form(form);
+    auto resp = http.request(url, verb, body, args);
+
+    if (resp->payload.empty())
+    {
+        // 响应为空
+        return -1;
+    }
+
+    rapidjson::Document doc;
+    doc.ParseInsitu(resp->payload.data());
+
+    if (doc.HasParseError())
+    {
+        // 响应内容不是合法 JSON
+        return -1;
+    }
+
+    int code = get(doc, "code");
+
+    if ((resp->statusCode / 100) != 2 || code != 0)
+    {
+        // 服务端返回错误
+        return make_int(resp->statusCode, get(doc, "code"));
+    }
+
+    return handler(doc["data"]);
 }
 
 
@@ -81,24 +178,37 @@ int kaixin_initialize(const char *app_key, const char *app_secret, const char *b
         return EINVAL;
     }
 
+    if (!is_empty(base_url) && strstr(base_url, "https://") != base_url)
+    {
+        // 基础 URL 必须是 HTTPS 协议
+        return EINVAL;
+    }
+
     g_config = new Config;
     _ASSERT(g_config != nullptr);
     g_config->app_key = app_key;
     g_config->app_secret = app_secret;
     g_config->base_url = base_url;
 
+    ix::initNetSystem();
     return 0;
 }
 
 
 void kaixin_uninitialize()
 {
+    ix::uninitNetSystem();
     delete g_config;
     g_config = nullptr;
 }
 
 
+static int sign_in_handler(const rapidjson::Value &data)
+{
+    return 0;
+}
+
 int kaixin_sign_in(const char *username, const char *password)
 {
-
+    return send_request(ix::HttpClient::kPost, "/session", {}, {}, sign_in_handler);
 }
---
+++ src/rapidjsonhelpers.h
@@ -0,0 +1,145 @@
+﻿/*! ***********************************************************************************************
+ *
+ * \file        rapidjsonhelpers.h
+ * \brief       rapidjson 辅助函数头文件。
+ *
+ * \version     0.1
+ * \date        2020-7-28
+ *
+ * \author      Roy QIU <karoyqiu@gmail.com>
+ * \copyright   © 2020 开心网络。
+ *
+ **************************************************************************************************/
+#pragma once
+#include <rapidjson/document.h>
+
+
+RAPIDJSON_NAMESPACE_BEGIN
+
+
+struct ValueTypeConverter
+{
+    ValueTypeConverter(const Value &v) : value(v) { }
+    const Value &value;
+
+    template<typename T>
+    operator T() const
+    {
+        if (value.Is<T>())
+        {
+            return value.Get<T>();
+        }
+
+        return T();
+    }
+
+    operator std::string() const
+    {
+        if (value.IsString())
+        {
+            return std::string(value.GetString(), value.GetStringLength());
+        }
+
+        return {};
+    }
+};
+
+
+#pragma region 按名称获取 JSON 对象的字段
+
+template<typename T>
+inline typename std::enable_if_t<!std::is_enum_v<T>, bool> get(T &t, const Value &j, const char *name)
+{
+    if (j.IsObject())
+    {
+        auto iter = j.FindMember(name);
+
+        if (iter != j.MemberEnd() && iter->value.Is<T>())
+        {
+            t = iter->value.Get<T>();
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+template<typename T>
+inline typename std::enable_if_t<std::is_enum_v<T>, bool> get(T &t, const Value &j, const char *name)
+{
+    return get(reinterpret_cast<std::underlying_type_t<T> &>(t), j, name);
+}
+
+
+template<>
+inline bool get(double &t, const Value &j, const char *name)
+{
+    if (j.IsObject())
+    {
+        auto iter = j.FindMember(name);
+
+        if (iter != j.MemberEnd())
+        {
+            if (iter->value.IsLosslessDouble())
+            {
+                t = iter->value.GetDouble();
+                return true;
+            }
+
+            if (iter->value.IsString())
+            {
+                t = std::strtod(iter->value.GetString(), nullptr);
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+
+template<>
+inline bool get(float &t, const Value &j, const char *name)
+{
+    if (j.IsObject())
+    {
+        auto iter = j.FindMember(name);
+
+        if (iter != j.MemberEnd())
+        {
+            if (iter->value.IsLosslessFloat())
+            {
+                t = iter->value.GetFloat();
+                return true;
+            }
+
+            if (iter->value.IsString())
+            {
+                t = std::strtof(iter->value.GetString(), nullptr);
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+
+template<typename T>
+inline typename T get(const Value &j, const char *name)
+{
+    T value = {};
+    get(value, j, name);
+    return value;
+}
+
+inline ValueTypeConverter get(const Value &j, const char *name)
+{
+    return ValueTypeConverter(j[name]);
+}
+
+#pragma endregion
+
+
+RAPIDJSON_NAMESPACE_END
